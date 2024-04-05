38a7db6092545f0ddfed79844a1870333e59231a
chaeunho1227 <chaeunho2002@gmail.com>
2024-04-05 16:00:47 +0900

    feat : model 적용 완료

--- project/main/models.py
+++ project/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    writer = models.CharField(max_length=30)
+    body = models.TextField()
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
